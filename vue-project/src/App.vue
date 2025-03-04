9bb5d4d7962cca83619d394e55e90ad57e0398c1
Rainier Edward Lopez <155550561+rainieredward@users.noreply.github.com>
2025-03-04 19:07:42 +0800

    adding component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <h1>Hello Kendrick Lamar World!</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
 <script></script>
-<style></style>
+
+<style>
+  #app > div {
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+</style> 

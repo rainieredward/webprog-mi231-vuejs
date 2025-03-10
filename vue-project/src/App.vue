2862239142f8c110364e26d82570f50638edb030
Rainier Edward Lopez <155550561+rainieredward@users.noreply.github.com>
2025-03-10 10:46:50 +0800

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <h1>Food</h1>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
+  <h1>Food</h1> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <personal-profile/> <br>
+
 </template>
 
 <script></script>
